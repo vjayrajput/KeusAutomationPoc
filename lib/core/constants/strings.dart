558bcc46c3a0a8221db41753173ad5d8e07619ab
Rajput Vijay <vjayrajput@gmail.com>
2024-09-30 12:15:07 +0530

    created asset image folder

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,40 @@
+class Strings {
+  Strings._();
+
+  static const String defaultCurrency = "\$";
+  static const String foodUI = "Food UI";
+  static const String salads = "Salads";
+  static const String pizza = "Pizza";
+  static const String beverages = "Beverages";
+  static const String snacks = "Snacks";
+  static const String hitsOfTheWeek = "Hits of the week";
+  static const String dummyTitle = "100a Ealing Rd • 24 mins";
+  static const String dummyAddress = "100a Ealing Rd • 24 mins";
+  static const String dummyDescription =
+      "Famous Hawaiian dish. Rice pillow with tender chicken breast, mushrooms, lettuce, cherry tomatoes, seaweed and cron, with unagi sauce.";
+  static const String weWillDeliverIn = "We will deliver in";
+  static const String minutesToTheAddress = "24 minutes to the address:";
+  static const String changeAddress = "Change address";
+  static const String cutlery = "Cutlery";
+  static const String delivery = "Delivery";
+  static const String freeDeliveryFrom = "Free delivery from \$30";
+  static const String paymentMethod = "Payment method";
+  static const String applePay = "Apple Pay";
+  static const String pay = "Pay";
+  static const String inMinutes = "24 min • ";
+  static const String cart = "Cart";
+  static const String addIn = "Add in";
+  static const String addToCart = "Add to cart";
+  static const String addressTitlePressed = "Address Title Pressed";
+  static const String searchIconPressed = "Search Icon Pressed";
+  static const String filterIconPressed = "Filter Icon Pressed";
+  static const String addInProductPressed = "Add extra in product Pressed";
+  static const String changeAddressPressed = "Change Address Pressed";
+  static const String paymentMethodPressed = "Payment Method Pressed";
+  static const String payCheckoutPressed = "Pay to Checkout Pressed";
+
+  static const String networkErrorOccurred = "Network error occurred";
+  static const String serverErrorOccurred = "Server error occurred";
+  static const String serverRespondedWithError = "Server responded with an error.";
+  static const String noInternetConnectionPleaseTryAgain = "No internet connection. Please try again.";
+}
